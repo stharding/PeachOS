5239838eb37a0b6193668a0e4a2fb7bebc350805
Stephen Harding <harding@spawar.navy.mil>
2021-09-14 14:32:37 -0700

    implemented interupt zero (the divide by zero exception handler)

--- boot.asm
+++ boot.asm
@@ -11,6 +11,15 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
+handle_zero:
+    ; mov ah, 0eh
+    ; mov al, 'A'
+    ; mov bx, 0x00
+    ; int 0x10
+    mov si, error_message
+    call print
+    iret
+
 step2:
     cli ; clear interupts
     mov ax, 0x7c0
@@ -19,8 +28,14 @@ step2:
     mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00
-
     sti ; enables interupts
+
+    mov word[ss:0x00], handle_zero
+    mov word[ss:0x02], 0x7c0
+
+    mov ax, 0
+    div ax
+
     mov si, message
     call print
     jmp $
@@ -43,6 +58,8 @@ print_char:
     ret
 
 message: db 'Hello World!', 0
+
+error_message: db 'Divide by zero! ', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
