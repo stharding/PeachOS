2c8764bf1ae6e7dab9aeca0638ac84bf57c5ec4d
Stephen Harding <harding@spawar.navy.mil>
2021-09-15 22:19:20 -0700

    programmable interrupt controller (end of lesson 31)

--- src/kernel.asm
+++ src/kernel.asm
@@ -20,6 +20,20 @@ _start:
     or al, 2
     out 0x92, al
 
+    ; remap the master PIC (Programmable Interrupt Controller)
+    mov al, 00010001b
+    out 0x20, al ; tell master PIC
+
+    mov al, 0x20 ; interrupt -x20 is where master IST should start
+    out 0x21, al
+
+    mov al, 00000001b
+    out 0x21, al
+    ; end remap of the master PIC
+
+    ; enable interrupts
+    sti
+
     call kernel_main
     jmp $
 
